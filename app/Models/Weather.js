e45c247083c578b7fe88d5d376e18b69ff45c148
Gage Lasher <rsgage21@gmail.com>
2022-02-25 16:26:11 -0700

    commit

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -7,13 +7,14 @@ export class Weather {
     get Template(){
         return `
           <div class="image-text" onclick="app.weatherController.clicked()">  ${this.ConvertedTemp} <span>${this.clicked ? "C" : "F"}</span> </div>
+          <div class="image-text"> ${this.city} </div>
         `
     }
     get ConvertedTemp(){
         let out = Math.round(1.8 * (this.temp - 273) + 32)
         // 1.8(this.temp - 273) + 32
         if (this.clicked) {
-            out = Math.floor(this.temp - 273)
+            out = Math.round(this.temp - 273)
         } else {
             out =  Math.round(1.8 * (this.temp - 273) + 32)
         }
