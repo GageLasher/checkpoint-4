ba3f18f682316688efdc3a025ef37f1747613a3a
Gage Lasher <rsgage21@gmail.com>
2022-02-25 16:15:23 -0700

    commit

--- app/Controllers/ImageController.js
+++ app/Controllers/ImageController.js
@@ -10,6 +10,23 @@ async function _getImage(){
     
 }
 
+function getTime(){
+    const d = new Date()
+  
+    let hour = d.getHours()
+    let minute = d.getMinutes()
+    if(hour > 12) {
+        hour -= 12
+    }
+    hour = hour < 10 ? "0" + hour : hour
+    minute = minute < 10 ? "0" + minute : minute
+    let currentTime = `${hour} : ${minute}`
+    console.log(currentTime);
+    document.getElementById("clock").innerText = currentTime
+}
+
+setInterval(getTime, 1000)
+
 function _draw(){
     document.body.style.backgroundImage = ProxyState.image.imgUrl
 }
@@ -18,5 +35,6 @@ export class ImageController{
     constructor(){
         _getImage()
         _draw()
+        getTime()
     }
 }
